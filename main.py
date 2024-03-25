f5adfb05b359ed3313136a1add345825fe70dcd7
Spartix <77485560+Spartix@users.noreply.github.com>
2024-03-25 18:28:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request,g,url_for,make_response
 import sqlite3
-from .src.users import user
+from src.users import user
 conn = sqlite3.connect('DB_CryptoGame.db')
 cursor = conn.cursor()
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('DB_CryptoGame.db')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def index():
@@ -15,14 +27,14 @@ def index():
 def login_page():
     return render_template("login.html")
 
-@app.route("/aysnc-register")
-def register_page():
-    name = request.form['nom']
-    prenom = resquest.form['prenom']
-    email = request.form['email']
-    mdp = request.form['password']
-    confirm_mdp = request.form[confirm password]
-    return render_template("register.html")
+# @app.route("/aysnc-register")
+# def register_page():
+#     name = request.form['nom']
+#     prenom = resquest.form['prenom']
+#     email = request.form['email']
+#     mdp = request.form['password']
+#     confirm_mdp = request.form[confirm password]
+#     return render_template("register.html")
 
 @app.route("/register")
 def register_page():
@@ -30,10 +42,15 @@ def register_page():
 
 @app.route("/async-login",methods=['POST'])
 def connexion():
-
-    if user.IsValid(request.form["username"],request.form["password"]):
-        token = user.GetToken(request.form["username"],request.form["password"])
-        return redirect("/putit",token=token)
+    with get_db() as conn:
+        cursor = conn.cursor()
+        if user.IsValid(request.form["username"],request.form["password"],cursor):
+            token = user.GetToken(request.form["username"],request.form["password"],cursor)
+            response = make_response(render_template('index.html'))
+            response.set_cookie('access_token', token)
+            return response
+        else:
+            return redirect("/login?error=true")
 
 # @app.route("/exemple")
 # def exemple():
--- src/users.py
+++ src/users.py
@@ -1,12 +1,10 @@
 import sqlite3
 import jwt
 
-conn = sqlite3.connect('DB_CryptoGame.db')
-cursor = conn.cursor()
-
+SECRET_KEY = '"PasTresSecretLaCleSecrete"'
 class user:
 
-    def IsValid(mail,mdp):
+    def IsValid(mail,mdp,cursor):
         print("je me connecte")
         assert type(mail) == str, "l'e-mail est invalide"
         assert '@' and '.' in mail,"l'e-mail est invalide"    
@@ -16,16 +14,30 @@ class user:
         cursor.execute(f"select ID_User FROM users where email = '{mail}' and pass = '{mdp}'")
         response = cursor.fetchall()
         print(response) # Le reponse contient donc tous les utilisateurs
-        return response == []
+        return response != []
 
-    def IsLogin(token):
+    def IsLogin(token,cursor):
         cursor.execute(f"select ID_User FROM users where Token = '{token}'")
         response = cursor.fetchall()
-        return response == []
+        return response != []
 
-    def GetToken(user,pas):
-        cursor.execute(f"select ID_User FROM users where email = '{user}' and pass = '{pas}'")
+    def GetToken(email,pas,cursor):
+        cursor.execute(f"select ID_User FROM users where email = '{email}' and pass = '{pas}'")
         response = cursor.fetchall()
-        if(user.IsValid(user,pas)):
-            encoded_jwt = jwt.encode({"username": user,"ID":response[0][0]}, "PasTresSecretLaCleSecrete", algorithm="HS256")
-        return response[0][0]
+        if(user.IsValid(email,pas,cursor)):
+            print(response[0])
+            # encoded_jwt = JWT.encode({"username": email,"ID":response[0][0]}, "PasTresSecretLaCleSecrete", algorithm="HS256")
+            accesstoken = user.encode_token({"username": email,"ID":response[0][0]})
+            #need to add the access token to database
+        return accesstoken
+
+    # Secret key for encoding and decoding JWT tokens
+    
+
+    def encode_token(payload):
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return token  # Convert bytes to string
+
+    def decode_token(token):
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        return payload
